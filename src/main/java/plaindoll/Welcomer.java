14ad460f454b733ed52488186145b16d6ce95ee7
aos <aos@vm1.myguest.virtualbox.org>
2025-05-08 00:19:07 +0500

    mission-12

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,4 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+	public String sayNeedHunter(){
+		return "netology hw-05-teamcity mission-10 HUNTER";
+	}
 }
--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -30,4 +30,8 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+	@Test
+	public void welcomerSaysSpoiler(){
+		assertThat(welcomer.sayNeedHunter(), containsString("HUNTER"));
+	}
 }
